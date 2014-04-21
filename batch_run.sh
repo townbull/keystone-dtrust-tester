1e178241cd69c72a7c7ebaafbf32b0b1b8a7c485
Bo Tang <botang.usa@gmail.com>
2014-04-21 17:25:25 -0500

    Added -t in the remote ssh execution
    The remote execution not exit problem solved.

--- batch_run.sh
+++ batch_run.sh
@@ -11,9 +11,8 @@ KS4="10.245.123.54"
 # Database server for experiment result storage
 DB="10.245.122.14"
 
-KSRST="cd /opt/stack/keystone;git pull;nohup ksrestart.sh &"
+KSRST="cd /opt/stack/keystone;git pull;./ksrestart.sh"
 
-ext=1
 ksx=2
 while [ $ksx -lt 3 ]
 do
@@ -21,55 +20,42 @@ do
     KSX="KS$ksx"
     KSX=${!KSX}
 
+    ext=6
     while [ $ext -le 10 ]
     do
         # restart expriment keystone server
-        ssh -o "StrictHostKeyChecking no" root@$KSX $KSRST
-	echo "done restart keystone"
+        ssh -t -o "StrictHostKeyChecking no" root@$KSX "$KSRST"
+        sleep 5
+ 
         # execute intra-domain experiment
         /opt/stack/keystone/run_exp.sh $ksx -i $ext
-	echo "sent request $ksx-I$ext-xxx."
         
         # wait result arrive the db
-        exp_num= $(($ext * 100))
+        exp_num=$(( $ext * 100 ))
         resp_num=0
-        timeout=0
-        while [ $resp_num -lt $exp_num ]
+        while [[ $resp_num < $exp_num ]]
         do
-            sleep 10
-            resp_num=$(/usr/bin/mysql -uroot -padmin -h$DB -N -e "use osacdt;select count(*) \
+            sleep 6
+            resp_num=$(mysql -uroot -padmin -h$DB -N -e "use osacdt;select count(*) \
                 from dt_exp_results where id like \"$ksx-I$ext-%\";")
-            resp_num=$(($resp_num))
-            timeout=$(($timeout + 1))
-            if [ $timeout -lt 100 ]
-            then
-                echo "Time out in $ksx-I$ext-xxx."
-                break
-            fi
+            echo "Response collected: $resp_num."
         done
 
         # restart expriment keystone server
-        ssh -o "StrictHostKeyChecking no" root@$KSX $KSRST
-
+        ssh -t -o "StrictHostKeyChecking no" root@$KSX "$KSRST"
+        sleep 5
 
         # execute cross-domain experiment
-        ./run_exp.sh $ksx -c $ext
+        /opt/stack/keystone/run_exp.sh $ksx -c $ext
 
         # wait result arrive the db
         resp_num=0
-        timeout=0
-        while [ $resp_num -lt $exp_num ]
+        while [[ $resp_num < $exp_num ]]
         do
-            sleep 10
+            sleep 6
             resp_num=$(/usr/bin/mysql -uroot -padmin -h$DB -N -e "use osacdt;select count(*) \
                 from dt_exp_results where id like \"$ksx-C$ext-%\";")
-            resp_num=$(($resp_num))
-            timeout=$(($timeout + 1))
-            if [ $timeout -lt 100 ]
-            then
-                echo "Time out in $ksx-C$ext-xxx."
-                break
-            fi
+            echo "Response collected: $resp_num."
         done
 
         ext=`expr $ext + 1`
